1a989ce414e94830ea5dfe97ad1c474290a9b54c
eyerusalemdani <eyerusalemdani@gmail.com>
2021-06-05 23:25:00 -0700

    HW1 first commit

---
+++ app/src/main/java/us/briksoft/hw1_first_android_project/MainActivity.java
@@ -0,0 +1,14 @@
+package us.briksoft.hw1_first_android_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
